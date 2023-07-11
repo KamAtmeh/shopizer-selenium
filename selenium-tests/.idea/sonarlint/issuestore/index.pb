f744dd13f1884a6496e934aa847e00d895eca169
titano-loris <loris.bartolini@orange.fr>
2023-07-11 12:09:13 +0200

    first commit

---
+++ selenium-tests/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+P
+ src/test/java/test_selenium.java,9\2\9230a905c5aa71e66ec869f409788ffb44bdbc5f
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ selenium-tests/src/test/java/test_selenium.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class test_selenium {
+
+    @Test
+    public void test_autom(){
+
+
+    }
+}
