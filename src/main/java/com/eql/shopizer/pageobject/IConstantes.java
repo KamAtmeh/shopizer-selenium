fc79f88651bc392ea147e9c5a7327fac1dfd363f
KAMALA <kamal.atmeh@hotmail.com>
2023-07-15 11:47:09 +0200

    Modification IConstantes

--- src/main/java/com/eql/shopizer/pageobject/IConstantes.java
+++ src/main/java/com/eql/shopizer/pageobject/IConstantes.java
@@ -9,13 +9,13 @@ public interface IConstantes {
 
     // Définition des variables de connexion
     Properties prop = initProp("src/main/resources/data/variablesGlobales.properties");
-    String NAVIGATEUR = prop.getProperty("NAVIGATEUR");
-    String URL = prop.getProperty("URL");
-    Boolean MAXIMIZE_DRIVER = Boolean.parseBoolean(prop.getProperty("MAXIMIZE_DRIVER"));
-    Boolean HEADLESS = Boolean.parseBoolean(prop.getProperty("HEADLESS"));
-    Integer IMP_WAIT = Integer.parseInt(prop.getProperty("IMP_WAIT"));
-    Duration EXP_WAIT = Duration.ofSeconds(Integer.parseInt(prop.getProperty("EXP_WAIT")));
-    String WINDOW_POS = prop.getProperty("WINDOW_POS");
-    String WINDOW_SIZE = prop.getProperty("WINDOW_SIZE");
+    String NAVIGATEUR = System.getProperty("NAVIGATEUR", prop.getProperty("NAVIGATEUR"));
+    String URL = System.getProperty("URL", prop.getProperty("URL"));
+    Boolean HEADLESS = Boolean.parseBoolean(System.getProperty("HEADLESS", prop.getProperty("HEADLESS")));
+    Boolean MAXIMIZE_DRIVER = Boolean.parseBoolean(System.getProperty("MAXIMIZE_DRIVER", prop.getProperty("MAXIMIZE_DRIVER")));
+    Integer IMP_WAIT = Integer.parseInt(System.getProperty("IMP_WAIT", prop.getProperty("IMP_WAIT")));
+    Duration EXP_WAIT = Duration.ofSeconds(Integer.parseInt(System.getProperty("EXP_WAIT", prop.getProperty("EXP_WAIT"))));
+    String WINDOW_POS = System.getProperty("WINDOW_POS", prop.getProperty("WINDOW_POS"));
+    String WINDOW_SIZE = System.getProperty("WINDOW_SIZE", prop.getProperty("WINDOW_SIZE"));
 
 }
